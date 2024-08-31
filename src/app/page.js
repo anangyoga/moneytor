2fd10688130d4ba4ec93683a0f7e2a3eddf0fda6
anangyoga <aharwinto@gmail.com>
2024-08-31 17:01:24 +0700

    fixL layouting color & height

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,11 @@ import BalanceTransaction from "@/components/BalanceTransaction";
 
 export default function Home() {
   return (
-    <main className="bg-primary h-screen w-screen text-white">
-      <section className="max-w-md mx-auto py-10 flex flex-col space-y-4">
+    <main>
+      <section className="max-w-md mx-auto py-10 flex flex-col space-y-4 px-3 sm:px-0 ">
         <Balance />
         <BalanceTransaction />
+        <hr />
         <BalanceCard />
       </section>
     </main>
