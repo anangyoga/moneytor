72d47bfb8cf816cad36cf9c20e47810ea5096d53
anangyoga <aharwinto@gmail.com>
2024-09-02 14:36:09 +0700

    fix: create convert-date utils

--- src/components/BalanceItem.jsx
+++ src/components/BalanceItem.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { formateDate } from "@/utils/formatDate";
 import { useRouter } from "next/navigation";
 
 export const BalanceItem = ({ id, description, amount, createdAt }) => {
@@ -23,7 +24,7 @@ export const BalanceItem = ({ id, description, amount, createdAt }) => {
         <p className="font-semibold">{description}</p>
         <p className="font-semibold">Rp {amount}</p>
       </div>
-      <p className="text-gray-400 font-thin text-sm">{createdAt}</p>
+      <p className="text-gray-400 font-thin text-sm">{formateDate(createdAt)}</p>
       <button className="self-end text-sm text-danger" onClick={() => handleDelete(id)}>
         Delete
       </button>
---
+++ src/utils/formatDate.js
@@ -0,0 +1,9 @@
+export const formateDate = (timestamp) => {
+  const date = new Date(timestamp);
+
+  const options = { year: "numeric", month: "long", day: "numeric" };
+
+  const formattedDate = date.toLocaleDateString("en-US", options);
+
+  return formattedDate;
+};
